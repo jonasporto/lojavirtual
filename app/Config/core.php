ef30c892a87f43c48ca8d822bcd25d3273e8bff2
jonasporto <jporto.contato@hotmail.com>
2014-03-09 15:00:23 -0300

    iniciando ambiente

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi2014');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768593096574535424967496836452014');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -269,7 +269,7 @@
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('UTC');
 
 /**
  *
